fd401b60c2070f0214b90ad4043f3cd73b6805ae
Ryan Icasiano <bos_tsip@yahoo.com>
2011-04-11 08:52:38 +0800

    test

---
+++ application/logs/2011/04/05.php
@@ -0,0 +1,5 @@
+<?php defined('SYSPATH') or die('No direct script access.'); ?>
+
+2011-04-05 01:42:40 --- ERROR: HTTP_Exception_404 [ 404 ]: The requested URL sapakan/index was not found on this server. ~ SYSPATH/classes/kohana/request/client/internal.php [ 94 ]
+2011-04-05 02:16:06 --- ERROR: HTTP_Exception_404 [ 404 ]: The requested URL welcoe/index was not found on this server. ~ SYSPATH/classes/kohana/request/client/internal.php [ 94 ]
+2011-04-05 02:17:02 --- ERROR: HTTP_Exception_404 [ 404 ]: The requested URL sapakan/index was not found on this server. ~ SYSPATH/classes/kohana/request/client/internal.php [ 94 ]
